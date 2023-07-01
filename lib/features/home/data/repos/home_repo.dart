942feac3b795ac12ba128295a853a709fdd70662
Rowan <rowanibrahem2003@gmail.com>
2023-06-30 19:45:51 -0700

    failure file

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,4 +1,9 @@
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/errors/failures.dart';
+
 abstract class Homeepo{
-  fetchBestSellerBooks();
-  fetchFeaturedBooks();
+  Future<Either<Failure,List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failure,List<BookModel>>> fetchFeaturedBooks();
 }
