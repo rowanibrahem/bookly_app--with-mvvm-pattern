e636c763d13a55fedbe1eb6c697416b6f848c8f7
Rowan <rowanibrahem2003@gmail.com>
2023-08-08 16:19:39 -0700

    add name

--- lib/features/home/presentation/views/widgets/best_seller.dart
+++ lib/features/home/presentation/views/widgets/best_seller.dart
@@ -74,66 +74,66 @@ class BookListViewItem extends StatelessWidget {
     );
   }
 }
- Shimmer getShimmerLoading(){
-  return Shimmer.fromColors(
-      baseColor: Colors.grey[300]!,
-      highlightColor: Colors.grey[100]!,
-    child: GestureDetector(
-      onTap: (){
-        GoRouter.of(context).push('/BookView' , extra: bookModel);
-      },
-      child: SizedBox(
-        height: 125,
-        child: Row(
-          children: [
-            CustomBookImage(imageUrl: bookModel.volumeInfo.imageLinks?.thumbnail??''),
-            const SizedBox(
-              width: 30,
-            ),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children:  [
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * .5,
-                    child:  Text (
-                      bookModel.volumeInfo.title!,
-                      style: Styles.textStyle20.copyWith(fontFamily: kGtSectraFine),
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 3,
-                  ),
-                  Text(
-                    bookModel.volumeInfo.authors![0],
-                    style: Styles.textStyle14,
-                  ),
-                  const SizedBox(
-                    height: 3,
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        'Free',
-                        style: Styles.textStyle20.copyWith(fontWeight: FontWeight.bold),
-                      ),
-                      Spacer(),
-                      Expanded(
-                          child: BookRating(
-                            rating: bookModel.volumeInfo.averageRating ?? 4,
-                            count: bookModel.volumeInfo.ratingsCount  ?? 2500,
-                          )
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );,
-  );
- }
+ // Shimmer getShimmerLoading(){
+ //  return Shimmer.fromColors(
+ //      baseColor: Colors.grey[300]!,
+ //      highlightColor: Colors.grey[100]!,
+ //    child: GestureDetector(
+ //      onTap: (){
+ //        GoRouter.of(context).push('/BookView' , extra: bookModel);
+ //      },
+ //      child: SizedBox(
+ //        height: 125,
+ //        child: Row(
+ //          children: [
+ //            CustomBookImage(imageUrl: bookModel.volumeInfo.imageLinks?.thumbnail??''),
+ //            const SizedBox(
+ //              width: 30,
+ //            ),
+ //            Expanded(
+ //              child: Column(
+ //                crossAxisAlignment: CrossAxisAlignment.start,
+ //                children:  [
+ //                  SizedBox(
+ //                    width: MediaQuery.of(context).size.width * .5,
+ //                    child:  Text (
+ //                      bookModel.volumeInfo.title!,
+ //                      style: Styles.textStyle20.copyWith(fontFamily: kGtSectraFine),
+ //                      maxLines: 2,
+ //                      overflow: TextOverflow.ellipsis,
+ //                    ),
+ //                  ),
+ //                  const SizedBox(
+ //                    height: 3,
+ //                  ),
+ //                  Text(
+ //                    bookModel.volumeInfo.authors![0],
+ //                    style: Styles.textStyle14,
+ //                  ),
+ //                  const SizedBox(
+ //                    height: 3,
+ //                  ),
+ //                  Row(
+ //                    children: [
+ //                      Text(
+ //                        'Free',
+ //                        style: Styles.textStyle20.copyWith(fontWeight: FontWeight.bold),
+ //                      ),
+ //                      Spacer(),
+ //                      Expanded(
+ //                          child: BookRating(
+ //                            rating: bookModel.volumeInfo.averageRating ?? 4,
+ //                            count: bookModel.volumeInfo.ratingsCount  ?? 2500,
+ //                          )
+ //                      ),
+ //                    ],
+ //                  ),
+ //                ],
+ //              ),
+ //            ),
+ //          ],
+ //        ),
+ //      ),
+ //    );,
+ //  );
+ // }
